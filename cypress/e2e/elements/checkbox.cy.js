43f1b8a01dc9283cccbc81512d12c71adb56ba09
Geovanny Aaya Jimenez <geoviolink@gmail.com>
2025-02-27 16:28:15 -0600

    Cambio de prueba

--- cypress/e2e/elements/checkbox.cy.js
+++ cypress/e2e/elements/checkbox.cy.js
@@ -63,8 +63,9 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         //Prueba de CI con Jenkins 2
         //Prueba de CI con Jenkins 3
         //Prueba de CI con Jenkins 4
-
-         //Prueba de CI con Jenkins 5 sin webhooks
+        //Prueba de CI con Jenkins 5 sin webhooks
+        //Prueba de CI con Jenkins 6 sin webhooks
+       
 
 
        
