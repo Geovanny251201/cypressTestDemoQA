835784125000d4c6c8d3402060754a4f5c85dbc3
Geovanny Aaya Jimenez <geoviolink@gmail.com>
2025-02-27 22:45:40 -0600

    Prueba con Self-Hosted Runner 10

--- cypress/e2e/elements/checkbox.cy.js
+++ cypress/e2e/elements/checkbox.cy.js
@@ -68,7 +68,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         //Prueba de CI con Jenkins 7 sin webhooks
         //Prueba con Self-Hosted Runner 8
         //Prueba con Self-Hosted Runner 9
-
+        //Prueba con Self-Hosted Runner 10
        
         
         
