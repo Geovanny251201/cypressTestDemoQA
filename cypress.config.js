0404eb7d4efbcbe829c790618b2fad063db9ef46
Geovanny Aaya Jimenez <geoviolink@gmail.com>
2025-04-04 18:07:26 -0600

    Configura múltiples reporters para Cypress y JUnit

--- cypress.config.js
+++ cypress.config.js
@@ -2,46 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      baseUrl: 'https://demoqa.com', // URL base de tus pruebas
-      reporter: 'mochawesome', // Habilita Mochawesome
-      reporterOptions: {
-      reportDir: 'cypress/reports', // Carpeta donde se guardarán los reportes
-      overwrite: false, // No sobrescribas reportes previos
-      html: true, // Generar reportes en HTML
-      json: true, // Generar reportes en JSON
-      },
-      video: true, // Grabar videos de las pruebas
-      screenshotOnRunFailure: true, // Capturar pantallas en caso de error
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-       specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
-       experimentalFetchPolyfill: false
-      
-   
-    },
-
+    baseUrl: "https://demoqa.com",
     viewportWidth: 2000,
     viewportHeight: 1500,
- 
-  },
-});
+    video: true,
+    screenshotOnRunFailure: true,
 
-
-
-
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://demoqa.com', // URL base de tus pruebas
-    reporter: 'mochawesome', // Habilita Mochawesome
+    reporter: "cypress-multi-reporters",
     reporterOptions: {
-      reportDir: 'cypress/reports', // Carpeta donde se guardarán los reportes
-      overwrite: false, // No sobrescribas reportes previos
-      html: true, // Generar reportes en HTML
-      json: true, // Generar reportes en JSON
+      configFile: "reporter-config.json"
     },
-    video: true, // Grabar videos de las pruebas
-    screenshotOnRunFailure: true, // Capturar pantallas en caso de error
+
+    setupNodeEvents(on, config) {
+      return config;
+    },
   },
 });
